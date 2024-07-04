694ce7e1c03ef27c663554bcd7455a67f0e3b39a
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-04 17:52:32 +0300

    places api

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,35 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import Map from '../components/Map'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import NavigateCard from '../components/NavigateCard'
+import RideOptionsCard from '../components/RideOptionsCard'
 const MapScreen = () => {
+  const Stack=createNativeStackNavigator()
   return (
     <View>
-      <Text>MapScreen</Text>
+     
+      <View className="h-1/2">
+<Map/>
+      </View>
+      <View className="h-1/2">
+<Stack.Navigator>
+  <Stack.Screen
+    name="NavigatorCard"
+    component={NavigateCard}
+    options={{
+      headerShown:false,
+    }}
+  />
+   <Stack.Screen
+    name="RideOptionsCard"
+    component={RideOptionsCard}
+    options={{
+      headerShown:false,
+    }}
+  />
+</Stack.Navigator>
+      </View>
     </View>
   )
 }
