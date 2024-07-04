f47c7fa644a87e1aedbaa58146e32f29964515cf
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-04 17:52:19 +0300

    homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,48 @@
 import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import NavOptions from '../components/navOptions'
-
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import {MAPS_API_KEY} from "@env"
+import { useDispatch } from 'react-redux'
+import { setDestination,setOrigin } from '../slices/navSlice'
 const HomeScreen = () => {
+  const dispatch= useDispatch()
   return (
     <SafeAreaView className="bg-white h-full">
     <View className="p-5">
         <Image
             source={{uri:"https://links.papareact.com/gzs"}}
             style={{width: 100, height: 100, resizeMode: 'contain', marginBottom: 20}}
+        />
+        <GooglePlacesAutocomplete
+        placeholder='Where from?'
+          nearbyPlacesApi="GooglePlacesSearch"
+          debounce={400}
+          styles={{
+            container:{
+              flex:0,
+            },
+            textInput:{
+              fontSize:18,
+            }
+          }}
+          onPress={(data,details=null)=>{
+    dispatch(setOrigin({
+      location:details.geometry.location,
+      description:data.description
+}))
+    dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key:MAPS_API_KEY,
+            language: 'en',
+            types: '(cities)',
+          
+          }}
         />
       <NavOptions/>   
     </View>
