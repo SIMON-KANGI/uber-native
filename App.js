d86103c691796a4fbb15bd79c5a2d7f583dea670
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-05 12:20:28 +0300

    added keybord effect

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform} from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
@@ -17,10 +17,16 @@ export default function App() {
     <Provider store={store}>
     <NavigationContainer>
       <SafeAreaProvider>
-      <Stack.Navigator>
+      <KeyboardAvoidingView style={{flex:1}}
+      behavior={Platform.OS ==="ios"?"padding":"height"}
+      keyboardVerticalOffset={Platform.OS ==="ios"?-64:0}
+      >
+           <Stack.Navigator>
         <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headerShown:false}}/>
         <Stack.Screen name='MapScreen' component={MapScreen} options={{headerShown:false}}/>
       </Stack.Navigator>
+   
+      </KeyboardAvoidingView>
    
    </SafeAreaProvider>
     </NavigationContainer>
