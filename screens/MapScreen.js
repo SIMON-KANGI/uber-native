60809c1dc852ef8cc90e57a108bb31b5464726d3
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-05 12:18:29 +0300

    maps api

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,13 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Map from '../components/Map'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import NavigateCard from '../components/NavigateCard'
 import RideOptionsCard from '../components/RideOptionsCard'
+import { Entypo } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'
 const MapScreen = () => {
   const Stack=createNativeStackNavigator()
+  const navigation=useNavigation()
   return (
     <View>
+    <TouchableOpacity 
+    onPress={()=>navigation.navigate('HomeScreen')}
+    className="absolute top-16 left-8 bg-gray-100 z-50 p-3 rounded-full shadow-lg">
+<Entypo
+  name="menu" size={24}
+/>
+    </TouchableOpacity>
      
       <View className="h-1/2">
 <Map/>
